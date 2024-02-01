6c907b4f3b01f244dfa585567bfeb8c6df6091fc
Simon Clavensjö <simon@clavensjo.se>
2024-02-01 09:31:08 +0100

    initial build
    Co-Authored-By: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
    Co-Authored-By: lukasgurra <142992155+lukasgurra@users.noreply.github.com>

---
+++ chatapp/src/app/ui/textForm.tsx
@@ -0,0 +1,11 @@
+export default function TextForm(){
+    return(
+        <>
+            <form>
+                <input type="text" id="Message" name="Message" className="text-gray-800"/>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
+
